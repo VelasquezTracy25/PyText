e241eddaa68dcbd6a4fe94d80db3ad075dcbd1f0
Tracy Velasquez <velasqueztracy25@gmail.com>
2023-08-31 11:26:35 -0500

    Add config

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from twilio.rest import Client
 import config
 
-client = Client(account_sid, auth_token)
-client1 = Client(account_sid, auth_token)
+client = Client(config.account_sid, config.auth_token)
+client1 = Client(config.account_sid, config.auth_token)
 
 
 call = client1.messages.create(
     to="12104730194",
     from_="+18664251786",
-    body="This is a test"
+    body="This is a test.. again"
 )
+
+print("Message Sent")
 
 # call.date
